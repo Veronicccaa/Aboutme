3d169955703917c0778562362a9c5733867badaa
veronica <Veronica0707@126.com>
2023-08-10 13:13:17 -0400

    Initial Commit

---
+++ Aboutme/AboutmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutmeApp.swift
+//  Aboutme
+//
+//  Created by scholar on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
